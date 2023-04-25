d4fd189177f71bf09e69d5f145be96657a23d21d
sasha <sashatv126@gmail.com>
2023-04-25 08:40:18 +0300

    #1 add splash screen

---
+++ Mum’s the word/Mum’s the word/Base/Constants/AppImages.swift
@@ -0,0 +1,13 @@
+//
+//  Images.swift
+//  Mum’s the word
+//
+//  Created by Александр Александрович on 25.04.2023.
+//
+
+import UIKit
+
+enum AppImages {
+    static let bg = UIImage(named: "Group 2849 (2)")
+    static let loader = UIImage(named: "launchScreen_loader")
+}
--- Mum’s the word/Mum’s the word/Common/Navigation/AppRouter.swift
+++ Mum’s the word/Mum’s the word/Common/Navigation/AppRouter.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class AppRouter {
     private let navigationRouter: NavigationRouterProtocol
+    private lazy var splashRouter: SplashRouter? = SplashRouter(navigationRouter: navigationRouter)
         
     init(navigationRouter: NavigationRouterProtocol) {
         self.navigationRouter = navigationRouter
@@ -19,5 +20,16 @@ extension AppRouter {
     func showOnboarding() {
         let router = OnboardingRouter(navigationRouter: navigationRouter)
         router.showOnboarding()
+    }
+    
+    func showSplash(scene: UIWindowScene) {
+        splashRouter?.scene = scene
+        splashRouter?.present()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            self.splashRouter?.dismiss(completion: { [weak self] in
+                self?.splashRouter = nil
+            })
+        }
     }
 }
